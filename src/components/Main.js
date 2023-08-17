f3e727cb87d935c03e368d3918da5acd0afa1719
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-08-17 21:34:10 +0900

    main css 에러 수정
    main css 에러 수정, 메인작업중

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import '../scss/_main.scss';
+
 function Main() {
 
     const url = process.env.PUBLIC_URL;
@@ -9,7 +12,7 @@ function Main() {
                     <div className="topText">안녕하세요! 김은지의 portfolio입니다.</div>
                     <h2>매일 발전해나가는 주니어 개발자입니다.</h2>
                     <div className="bottomText">어쩌구저쩌구</div>
-                    <a href="#test" className="Btn">see my project</a>
+                    <a href="#test" className="btn">see my project</a>
                     <div className="imgWrap">
                         <img src={`${url}/img/eunjeeKim.jpg`} alt="eunjee" />
                         <h3>이름~정보~~</h3>
