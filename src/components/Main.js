c9f8c1b1672c8b9890b89a73598ea442c16bfd46
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-08-22 16:27:07 +0900

    main 두번째화면 작업중
    main 두번째화면 작업중

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,7 +10,7 @@ function Main() {
             <section className="mainConts1">
                 <div className="inner">
                     <div className="topText">안녕하세요! 김은지의 portfolio입니다.</div>
-                    <h2>매일 발전해나가는 주니어 개발자입니다.</h2>
+                    <h2>매일 발전해나가는 <br></br>주니어 개발자입니다.</h2>
                     <div className="bottomText">어쩌구저쩌구</div>
                     <a href="#test" className="btn">see my project</a>
                     <div className="imgWrap">
@@ -18,6 +18,20 @@ function Main() {
                         <h3>이름~정보~~</h3>
                     </div>
                 </div>
+            </section>
+            <section className='mainConts2'>
+                <div className='inner'>
+                    <h1 className='tit'>My Skills</h1>
+                    <ul className='conts'>
+                        <li className='item'>
+                            <div className='top'>
+                                <span className='num'></span>
+                            </div>
+                            <h2>Frontend</h2>
+                            <p>jQuery Javascript...</p>                        
+                        </li>
+                    </ul>
+                </div>                
             </section>
         </>
 
