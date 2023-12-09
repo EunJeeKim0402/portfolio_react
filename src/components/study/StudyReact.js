672e210132f057cc7f61d34268dba80ad0e17045
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-12-09 14:28:49 +0900

    게시판 페이지 수정
    게시판 페이지 수정

--- src/components/study/StudyReact.js
+++ src/components/study/StudyReact.js
@@ -1,18 +1,84 @@
+import React from 'react';
 import StudyCategory from './StudyCategory';
+import Fade from 'react-reveal/Fade';
 
 function StudyReact() {
-    return(
-        <section className="studyConts">
-            <div className="inner">
-                <StudyCategory />
-                <main>
-                    <div>
-                        React페이지임
-                    </div>
-                </main>
+  return (
+    <section className="studyConts">
+      <div className="inner">
+        <StudyCategory />
+        <main>
+          <div>
+            <h1>React</h1>
+          </div>
+          <br />
+          <Fade>
+            <div className="boardConts">
+              <div>
+                <h6>호호</h6>
+              </div>
             </div>
-        </section>
-    )
+          </Fade>
+          <Fade delay={50}>
+            <div className="boardConts">
+              <div>
+                <h6>호호</h6>
+              </div>
+            </div>
+          </Fade>
+          <Fade delay={100}>
+            <div className="boardConts">
+              <div>
+                <h6>호호</h6>
+              </div>
+            </div>
+          </Fade>
+          <Fade delay={150}>
+            <div className="boardConts">
+              <div>
+                <h6>호호</h6>
+              </div>
+            </div>
+          </Fade>
+          <Fade delay={200}>
+            <div className="boardConts">
+              <div>
+                <h6>호호</h6>
+              </div>
+            </div>
+          </Fade>
+          <Fade delay={250}>
+            <div className="boardConts">
+              <div>
+                <h6>호호</h6>
+              </div>
+            </div>
+          </Fade>
+          <Fade delay={300}>
+            <div className="boardConts">
+              <div>
+                <h6>호호</h6>
+              </div>
+            </div>
+          </Fade>
+          <Fade delay={350}>
+            <div className="boardConts">
+              <div>
+                <h6>호호</h6>
+              </div>
+            </div>
+          </Fade>
+          <Fade delay={400}>
+            <div className="boardConts">
+              <div>
+                <h6>호호</h6>
+              </div>
+            </div>
+          </Fade>
+        </main>
+      </div>
+    </section>
+  );
 }
 
 export default StudyReact;
